4eaafb1d536c8ed75ad47775502ade480cc1d613
Anton Grönlund <anton.gronlund.2@wirelesscar.com>
2020-02-11 10:03:19 +0100

    Make project build locally

---
+++ service/src/test/java/com/wirelesscar/vw/tscs/functions/GetCertificatesTest.java
@@ -0,0 +1,15 @@
+package com.wirelesscar.vw.tscs.functions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GetCertificatesTest {
+
+    @Test
+    public void alwaysPassingTest() {
+
+        assertTrue(true);
+
+    }
+}
